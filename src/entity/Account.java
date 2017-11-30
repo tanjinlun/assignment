1c5b160efa30647dd29a90a24ff8c85db81cf926
vcterror <33213718+vcterror@users.noreply.github.com>
2017-11-30 10:42:01 +0800

    Add files via upload

---
+++ src/entity/Account.java
@@ -0,0 +1,46 @@
+package entity;
+
+public class Account {
+  private String name;
+  private String username;
+  private String password;
+
+  public Account(String name, String username, String password) {
+    this.name = name;
+    this.username = username;
+    this.password = password;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+      public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+ 
+    
+  @Override
+  public String toString() {
+    return String.format("%-20s %-20s %-20s ", name, username, password);
+  }
+  
+  
+}
