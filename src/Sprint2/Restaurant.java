d2351badd5bd97d0278f0a341ae00dee377983f6
tan <tan@ZhiHua>
2017-12-01 14:11:19 +0800

    Merge origin/ZhiHua-Branch into ZhiHua-Branch

---
+++ src/Sprint2/Restaurant.java
@@ -0,0 +1,39 @@
+package Sprint2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author taruc
+ */
+public class Restaurant {
+    private String place;
+    private String ResName;
+    
+    public Restaurant(String place,String ResName){
+        this.place = place;
+        this.ResName = ResName;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getResName() {
+        return ResName;
+    }
+
+    public void setResName(String ResName) {
+        this.ResName = ResName;
+    }
+    
+}
+
---
+++ src/Sprint2/place.java
@@ -0,0 +1,39 @@
+package Sprint2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tan
+ */
+public class place {
+    private String place;
+    private String ShopName;
+    
+    public place(String place, String ShopName){
+        this.place = place;
+        this.ShopName = ShopName;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getShopName() {
+        return ShopName;
+    }
+
+    public void setShopName(String ShopName) {
+        this.ShopName = ShopName;
+    }
+    
+    
+}
